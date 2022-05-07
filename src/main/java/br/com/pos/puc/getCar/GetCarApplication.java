711e5167db9ae473bbb008d0af8839826dda6d50
Carolini Moraes da Silva <carolinimo@hotmail.com>
2022-05-06 23:35:07 -0300

    Criação do projeto

---
+++ src/main/java/br/com/pos/puc/getCar/GetCarApplication.java
@@ -0,0 +1,13 @@
+package br.com.pos.puc.getCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pos/puc/getCar/GetCarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pos.puc.getCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
